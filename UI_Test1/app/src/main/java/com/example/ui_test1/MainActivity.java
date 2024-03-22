28ae5566fffea25e50a7e11113c50f662c1f10a9
yslin17 <betty20031217@gmail.com>
2024-03-22 13:38:13 +0800

    0322

--- UI_Test1/app/src/main/java/com/example/ui_test1/MainActivity.java
+++ UI_Test1/app/src/main/java/com/example/ui_test1/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.ui_test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_main);
+    }
+    public void sendString(View view) {
+        EditText edtInput = (EditText) findViewById(R.id.edtInput);
+        String string2send = edtInput.getText().toString();
+
+        Intent intent = new Intent( this, DisplayStringActivity.class);
+        intent.putExtra("userInput" , string2send);
+        startActivity(intent);
     }
 }
