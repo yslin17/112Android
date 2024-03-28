a61cdbd65291221b3b73487fb5e43bcb1175f96f
yslin17 <betty20031217@gmail.com>
2024-03-28 22:50:54 +0800

    0322

--- BMI/app/src/main/java/com/example/bmi/MainActivity.java
+++ BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.FeatureInfo;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -10,20 +9,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.security.interfaces.EdECKey;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private TextView txvShow = (TextView) findViewById(R.id.txvShow);
+    private TextView txvShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txvShow = (TextView) findViewById(R.id.txvShow);
         txvShow.setTextSize(36);
 
-        Button btnCalc = (Button) findViewById(R.id.btnColc);
+        Button btnCalc = (Button) findViewById(R.id.btnCalc);
         Button btnClear = (Button) findViewById(R.id.btnClear);
         btnCalc.setOnClickListener(this);
         btnClear.setOnClickListener(this);
@@ -31,24 +29,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        EditText edtHeight = (EditText)  findViewById(R.id.edtHeight);
-        EditText edtWeight = (EditText)  findViewById(R.id.edtWeight);
+        EditText edtHeight = (EditText) findViewById(R.id.edtHeight);
+        EditText edtWeight = (EditText) findViewById(R.id.edtWeight);
 
-        if(v.getId() == R.id.btnColc){
+        if (v.getId() == R.id.btnCalc)
+        {
             double height = Double.parseDouble(edtHeight.getText().toString());
             double weight = Double.parseDouble(edtWeight.getText().toString());
-            double bmi = weight / Math.pow(height  / 100.0 , 2);
-
-            if (bmi>=24){
+            double bmi = weight / Math.pow(height/100.0 , 2); //體重除以身高轉公尺的平方
+            if (bmi >= 24)
                 txvShow.setTextColor(Color.RED);
-            }else if(bmi<18.5) {
+            else if (bmi <18.5)
                 txvShow.setTextColor(Color.BLUE);
-            }else{
+            else
                 txvShow.setTextColor(Color.GREEN);
-            }
-            txvShow.setText(String.format("%.2f" , bmi));
+
+            txvShow.setText(String.format("%.2f, bmi"));
         }
-        else {
+        else
+        {
             edtHeight.setText("");
             edtWeight.setText("");
             txvShow.setText("");
