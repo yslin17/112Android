b8db9c5f714637139eb3a88a30475d9cecb7ac7d
yslin17 <betty20031217@gmail.com>
2024-04-12 13:42:04 +0800

    0412

---
+++ LifecycleTest/app/src/main/java/com/example/lifecycletest/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.lifecycletest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  final String TAG = "LitecycleTest";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        Log.v(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected  void onStart() {
+        super.onStart();
+        Log.v(TAG, "onStart");
+        super.onStart();
+    }
+    @Override
+    protected  void onRestart() {
+        super.onRestart();
+        Log.v(TAG, "onRestart");
+        super.onRestart();
+    }
+    @Override
+    protected  void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume");
+        super.onResume();
+    }
+    @Override
+    protected  void onPause() {
+        super.onPause();
+        Log.v(TAG, "onPause");
+        super.onPause();
+    }
+    @Override
+    protected  void onStop() {
+        super.onStop();
+        Log.v(TAG, "onStop");
+        super.onStop();
+    }
+    @Override
+    protected  void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy");
+        super.onDestroy();
+    }
+}
