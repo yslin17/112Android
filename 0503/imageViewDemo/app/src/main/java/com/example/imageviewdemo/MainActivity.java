bbc0b8ee1dc29c4c7144a701be89048827f3e4f3
yslin17 <betty20031217@gmail.com>
2024-05-03 14:31:43 +0800

    0503

--- 0503/imageViewDemo/app/src/main/java/com/example/imageviewdemo/MainActivity.java
+++ 0503/imageViewDemo/app/src/main/java/com/example/imageviewdemo/MainActivity.java
@@ -1,18 +1,46 @@
 package com.example.imageviewdemo;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.media.Image;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
+
 
 public class MainActivity extends AppCompatActivity {
-
     private ImageView image;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        image = (ImageView)  findViewById(R.id.imgOutput);
+        image = (ImageView) findViewById(R.id.imgOutput);
+        RadioGroup rg = (RadioGroup) findViewById(R.id.rgImages);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rdbDog)
+                {
+                    image.setImageResource(R.mipmap.dog);
+                }
+                else if (checkedId == R.id.rdbElephant)
+                {
+                    image.setImageResource(R.mipmap.elephant);
+                }
+                else if (checkedId == R.id.rdbMouse)
+                {
+                    image.setImageResource(R.mipmap.mouse);
+                }
+                else if (checkedId == R.id.rdbRabbit)
+                {
+                    image.setImageResource(R.mipmap.rabbit);
+                }
+            }
+        });
     }
 }
+
+
+
