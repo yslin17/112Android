5242a6d1145d1ac8997266cda95f689ca323a47d
yslin17 <betty20031217@gmail.com>
2024-05-03 14:24:20 +0800

    0503

--- 0503/imageViewDemo/app/src/main/java/com/example/imageviewdemo/MainActivity.java
+++ 0503/imageViewDemo/app/src/main/java/com/example/imageviewdemo/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.imageviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image = (ImageView)  findViewById(R.id.imgOutput);
     }
 }
