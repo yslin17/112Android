90746540d0f5f4b4ed9265e7ba5c6fb675522dad
yslin17 <betty20031217@gmail.com>
2024-05-10 14:18:39 +0800

    0510

--- MyApplication/0510/SpinnerAdapter/app/src/main/java/com/example/spinneradapter/MainActivity.java
+++ MyApplication/0510/SpinnerAdapter/app/src/main/java/com/example/spinneradapter/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.spinneradapter;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private  String[]course,desserts;
+    private Spinner spCourse,spDesserts;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        course=getResources().getStringArray(R.array.courses);
+        desserts=getResources().getStringArray(R.array.desserts);
+
+
+        spCourse=(Spinner)findViewById(R.id.spinner);
+        spDesserts=(Spinner) findViewById(R.id.spinner2);
+
+
+        ArrayAdapter<String>adpCourse=new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item,course);
+        ArrayAdapter<String>adpDessets=new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item,desserts);
+
+
+        spCourse.setAdapter(adpCourse);
+        spDesserts.setAdapter(adpDessets);
+
+
+        Button btnConfirm=(Button)findViewById(R.id.button);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                String course = spCourse.getSelectedItem().toString();
+                String dessert = spDesserts.getSelectedItem().toString();
+                TextView output=(TextView) findViewById(R.id.lblOutput);
+                output.setText("主餐"+course+"\n甜點:"+dessert);
+
+
+            }
+        });
     }
 }
+
