6c7dd04630a390af7a0b1bc6f54363cf578083d6
yslin17 <betty20031217@gmail.com>
2024-05-10 14:52:30 +0800

    0510

--- MyApplication/0510/ArrayAdapterSpinner/app/src/main/java/com/example/arrayadapterspinner/MainActivity.java
+++ MyApplication/0510/ArrayAdapterSpinner/app/src/main/java/com/example/arrayadapterspinner/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.arrayadapterspinner;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner drink,temp;
+    private TextView txv;
+    private  String[] tempSet1={"冰","去冰","溫"};
+    private  String[] tempSet2={"冰","去冰"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        txv=(TextView) findViewById(R.id.order);
+        temp=(Spinner)findViewById(R.id.temp);
+
+
+        drink=(Spinner)findViewById(R.id.drink);
+        drink.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[]tempSet;
+                if(position==3)
+                    tempSet=tempSet2;
+                else
+                    tempSet=tempSet1;
+
+
+                ArrayAdapter<String>tempAd=
+                        new ArrayAdapter<>(MainActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                tempSet);
+                tempAd.setDropDownViewResource(
+                        android.R.layout.simple_spinner_dropdown_item
+                );
+                temp.setAdapter(tempAd);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Button order=(Button)findViewById(R.id.btnOrder);
+        order.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String msg = drink.getSelectedItem().toString()+","+
+                        temp.getSelectedItem().toString();
+                txv.setText(msg);
+            }
+
+        });
     }
 }
+
