646a88f1107f61e3f3bfc2dc2d65779b80d8a5f1
yslin17 <betty20031217@gmail.com>
2024-06-07 13:02:26 +0800

    0607

--- 0607/ExplicitIntentDemo2/app/src/main/java/com/example/explicitintentdemo2/MainActivity.java
+++ 0607/ExplicitIntentDemo2/app/src/main/java/com/example/explicitintentdemo2/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.explicitintentdemo2;
 
+
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,43 +15,51 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
 
+
     private TextView output;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         output=(TextView) findViewById(R.id.lblOutput);
         Button button = (Button) findViewById(R.id.button);
+
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                EditText txtOpd1,txtOpd2;
-                txtOpd1=(EditText) findViewById(R.id.txtOpd1);
-                txtOpd2=(EditText) findViewById(R.id.txtOpd2);
-                Intent intent=new Intent(MainActivity.this,OpActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("OPERAND01",txtOpd1.getText().toString());
-                bundle.putString("OPERAND02",txtOpd2.getText().toString());
-                intent.putExtras(bundle);
-                startActivityForResult(intent,1);
+                //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
             }
         });
     }
-    @Override
-    protected void onActivityResult(int requestCode,int resultCode,Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        switch (requestCode){
-            case 1:
-                if(resultCode==RESULT_OK){
-                Bundle bundle = data.getExtras();
-                output.setText("計算結果:"+bundle.getDouble("RESULT"));
-            }
-            break;
+    class ResultContract extends ActivityResultContract<Boolean, String>{
+
+
+        @NonNull
+        @Override
+        public Intent createIntent(@NonNull Context context, Boolean aBoolean) {
+            Intent intent = new Intent(MainActivity.this,OpActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("OPERAND01", ((EditText) findViewById(R.id.txtOpd1)).getText().toString());
+            bundle.putString("OPERAND02", ((EditText) findViewById(R.id.txtOpd2)).getText().toString());
+            intent.putExtras(bundle);
+            return intent;
+        }
+
+
+        @Override
+        public String parseResult(int i, @Nullable Intent intent) {
+            Bundle bundle = intent.getExtras();
+            Double result = bundle.getDouble("RESULT");
+            return result.toString();
         }
     }
 }
+
